c7be2e607786efaef9639b1d8cef91251edb52b8
FabioCarvalho <fabiocarvalho.br@hotmail.com>
2021-05-14 15:19:50 -0300

    Aula09

--- aula09/spring03/src/main/java/br/carvalho/spring03/controller/EventoController.java
+++ aula09/spring03/src/main/java/br/carvalho/spring03/controller/EventoController.java
@@ -1,12 +1,21 @@
 package br.carvalho.spring03.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.carvalho.spring03.dto.ContagemAlarme;
 import br.carvalho.spring03.model.Evento;
 import br.carvalho.spring03.repo.EventoRepo;
 
@@ -23,6 +32,27 @@ public class EventoController {
         Evento evento = repo.findById(id).orElse(null);
 
         return evento;
+    }
+    @PostMapping("/data")
+    public List<Evento> buscarPorData(@RequestBody ObjectNode datas){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate dataini = LocalDate.parse(datas.get("dtIni").asText(), formato);
+        LocalDate datafim = LocalDate.parse(datas.get("dtFim").asText(), formato);
+        
+        List<Evento> lista = repo.findByDataEvtBetween(dataini, datafim);
+
+        return lista;
+    }
+    @PostMapping("/group/data")
+    public List<ContagemAlarme> contarPorData(@RequestBody ObjectNode datas){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate data = LocalDate.parse(datas.get("dtIni").asText(), formato);
+        
+        List<ContagemAlarme> lista = repo.countAlarmePorTipo(data);
+
+        return lista;
     }
 }
 
--- aula09/spring03/src/main/java/br/carvalho/spring03/repo/EventoRepo.java
+++ aula09/spring03/src/main/java/br/carvalho/spring03/repo/EventoRepo.java
@@ -1,8 +1,18 @@
 package br.carvalho.spring03.repo;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import br.carvalho.spring03.dto.ContagemAlarme;
 import br.carvalho.spring03.model.Evento;
 
 public interface EventoRepo extends CrudRepository<Evento, Integer> {
+    public List<Evento> findByDataEvtBetween (LocalDate ini, LocalDate fim);
+
+    @Query("Select new br.carvalho.spring03.dto.ContagemAlarme(e.alarme.nome, Count(e.alarme.nome)) From Evento as e where e.dataEvt = ?1 group by e.alarme.nome")
+    public List<ContagemAlarme> countAlarmePorTipo(LocalDate data);
 
 }
