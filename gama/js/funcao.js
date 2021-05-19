571e173e600124efeea42bf5f869203f1f419050
FabioCarvalho <fabiocarvalho.br@hotmail.com>
2021-05-19 17:51:04 -0300

    JavaScript - Desenvolvimento Web (FrontEnd)

---
+++ gama/js/funcao.js
@@ -0,0 +1,17 @@
+<script>
+
+/*Criando uma função de alert
+    alert ("Sejam todos bem vindos a Gama Academy");*/
+
+    /*var empresa = "Gama Academy";
+    var mensagem = "Sejam bem vindos a: ";
+    alert(mensagem + empresa);*/
+
+    /*var nome=prompt("Olá seja bem vindo a Gama Academy, qual o seu nome?", "Insire o seu nome");
+    alert ("Bem vindo " +nome);*/
+
+        function mensagem() {
+        alert("Para obter maiores informações acesse: www.gamaacademy.com.br")
+        }
+
+        </script>
